d0d071587f175efe15d92dd103d7f1d3e2f326ac
Papa Smurf <chris@violet.co>
2023-02-13 14:20:56 +0000

    fix: eatVerifier naming fixed to swapRouter

--- contracts/UniswapV3PoolDeployer.sol
+++ contracts/UniswapV3PoolDeployer.sol
@@ -30,10 +30,10 @@ contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
         address token1,
         uint24 fee,
         int24 tickSpacing,
-        address eatVerifier
+        address swapRouter
     ) internal returns (address pool) {
         parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
-        pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}(eatVerifier));
+        pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}(swapRouter));
         delete parameters;
     }
 }
