6d26bfce88e6cbe350db73202bbda499e1e43959
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-03-09 10:09:47 +0000

    set swapRouter and positionManager as swapTargetCallee

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -67,6 +67,11 @@ export const poolFixture: Fixture<PoolFixture> = async function (): Promise<Pool
   const swapTargetCallee = (await calleeContractFactory.deploy()) as TestUniswapV3Callee
   const swapTargetRouter = (await routerContractFactory.deploy()) as TestUniswapV3Router
 
+  // Set positionManager address
+  await factory.setPositionManager(swapTargetCallee.address)
+  // Set swapRouter address
+  await factory.setSwapRouter(swapTargetCallee.address)
+
   return {
     token0,
     token1,
@@ -75,7 +80,8 @@ export const poolFixture: Fixture<PoolFixture> = async function (): Promise<Pool
     swapTargetCallee,
     swapTargetRouter,
     createPool: async (fee, tickSpacing, firstToken = token0, secondToken = token1) => {
-      const mockTimePoolDeployer = (await MockTimeUniswapV3PoolDeployerFactory.deploy()) as MockTimeUniswapV3PoolDeployer
+      const mockTimePoolDeployer =
+        (await MockTimeUniswapV3PoolDeployerFactory.deploy()) as MockTimeUniswapV3PoolDeployer
       const tx = await mockTimePoolDeployer.deploy(
         factory.address,
         firstToken.address,
